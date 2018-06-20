544e66adb2b0648459ab0c3d28044d34129e51c5
naokikimura <n.kimura.cap@gmail.com>
2018-06-21 00:15:57 +0900

    Implement Formatter

--- lib/phc_string_format.rb
+++ lib/phc_string_format.rb
@@ -1,5 +1,54 @@
 require "phc_string_format/version"
+require "base64"
 
+#
+# PHC string format implemented by Ruby
+#
+#  ```
+#  $<id>[$<param>=<value>(,<param>=<value>)*][$<salt>[$<hash>]]
+#  ```
+#
 module PhcStringFormat
-  # Your code goes here...
+
+  module Formatter
+
+    #
+    def self.format(id:, params: {}, salt: '', hash: '')
+      raise ArgumentError.new, 'id is required' if id.nil?
+      raise ArgumentError.new, 'hash needs salt' if (salt.nil? || salt.empty?) && !(hash.nil? || hash.empty?)
+
+      elements = [
+        id,
+        params.entries.map { |k, v| "#{k}=#{v}" }.join(','),
+        short_strict_encode64(salt),
+        short_strict_encode64(hash)
+      ]
+      "$#{elements.select { |e| !(e.nil? || e.empty?) } .join('$')}"
+    end
+
+    #
+    def self.parse(string)
+      elements = string.split('$')
+      elements.shift
+      id = elements.shift
+      params = elements.shift.split(',').map {|e| e.split('=')}.reduce({}) {|h, e| k, v = e; h[k]=v; h}
+      salt = short_strict_decode64(elements.shift)
+      hash = short_strict_decode64(elements.shift)
+      {id: id, params: params, salt: salt, hash: hash }
+    end
+
+    def self.short_strict_encode64(bin)
+      return nil unless bin
+      Base64.strict_encode64(bin).delete('=')
+    end
+
+    def self.short_strict_decode64(bin)
+      return nil unless bin
+      Base64.strict_decode64(bin + '=' * (-bin.size % 4))
+    end
+
+    private_class_method :short_strict_encode64, :short_strict_decode64
+
+  end
+
 end
--- spec/phc_string_format_spec.rb
+++ spec/phc_string_format_spec.rb
@@ -4,6 +4,32 @@ RSpec.describe PhcStringFormat do
   end
 
   it "does something useful" do
-    expect(false).to eq(true)
+    test_cases = [
+      "$argon2i$m=120,t=5000,p=2",
+      "$argon2i$m=120,t=4294967295,p=2",
+      "$argon2i$m=2040,t=5000,p=255",
+      "$argon2i$m=120,t=5000,p=2,keyid=Hj5+dsK0",
+      "$argon2i$m=120,t=5000,p=2,keyid=Hj5+dsK0ZQ",
+      "$argon2i$m=120,t=5000,p=2,keyid=Hj5+dsK0ZQA",
+      "$argon2i$m=120,t=5000,p=2,data=sRlHhRmKUGzdOmXn01XmXygd5Kc",
+      "$argon2i$m=120,t=5000,p=2,keyid=Hj5+dsK0,data=sRlHhRmKUGzdOmXn01XmXygd5Kc",
+      "$argon2i$m=120,t=5000,p=2$/LtFjH5rVL8",
+      "$argon2i$m=120,t=5000,p=2$4fXXG0spB92WPB1NitT8/OH0VKI",
+      "$argon2i$m=120,t=5000,p=2$BwUgJHHQaynE+a4nZrYRzOllGSjjxuxNXxyNRUtI6Dlw/zlbt6PzOL8Onfqs6TcG",
+      "$argon2i$m=120,t=5000,p=2,keyid=Hj5+dsK0$4fXXG0spB92WPB1NitT8/OH0VKI",
+      "$argon2i$m=120,t=5000,p=2,data=sRlHhRmKUGzdOmXn01XmXygd5Kc$4fXXG0spB92WPB1NitT8/OH0VKI",
+      "$argon2i$m=120,t=5000,p=2,keyid=Hj5+dsK0,data=sRlHhRmKUGzdOmXn01XmXygd5Kc$4fXXG0spB92WPB1NitT8/OH0VKI",
+      "$argon2i$m=120,t=5000,p=2$4fXXG0spB92WPB1NitT8/OH0VKI$iPBVuORECm5biUsjq33hn9/7BKqy9aPWKhFfK2haEsM",
+      "$argon2i$m=120,t=5000,p=2,keyid=Hj5+dsK0$4fXXG0spB92WPB1NitT8/OH0VKI$iPBVuORECm5biUsjq33hn9/7BKqy9aPWKhFfK2haEsM",
+      "$argon2i$m=120,t=5000,p=2,data=sRlHhRmKUGzdOmXn01XmXygd5Kc$4fXXG0spB92WPB1NitT8/OH0VKI$iPBVuORECm5biUsjq33hn9/7BKqy9aPWKhFfK2haEsM",
+      "$argon2i$m=120,t=5000,p=2,keyid=Hj5+dsK0,data=sRlHhRmKUGzdOmXn01XmXygd5Kc$4fXXG0spB92WPB1NitT8/OH0VKI$iPBVuORECm5biUsjq33hn9/7BKqy9aPWKhFfK2haEsM",
+      "$argon2i$m=120,t=5000,p=2,keyid=Hj5+dsK0,data=sRlHhRmKUGzdOmXn01XmXygd5Kc$iHSDPHzUhPzK7rCcJgOFfg$EkCWX6pSTqWruiR0",
+      "$argon2i$m=120,t=5000,p=2,keyid=Hj5+dsK0,data=sRlHhRmKUGzdOmXn01XmXygd5Kc$iHSDPHzUhPzK7rCcJgOFfg$J4moa2MM0/6uf3HbY2Tf5Fux8JIBTwIhmhxGRbsY14qhTltQt+Vw3b7tcJNEbk8ium8AQfZeD4tabCnNqfkD1g",
+    ]
+    test_cases.each do |test_case|
+      phc_params = PhcStringFormat::Formatter.parse(test_case)
+      phc_string = PhcStringFormat::Formatter.format(phc_params)
+      expect(phc_string).to eq(test_case)
+    end
   end
 end
