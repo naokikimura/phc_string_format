961d8d384fcff36499dbfb1879948d59214edd19
naokikimura <n.kimura.cap@gmail.com>
2018-06-29 01:23:36 +0900

    Update phc_string_format.rb

--- lib/phc_string_format.rb
+++ lib/phc_string_format.rb
@@ -9,24 +9,20 @@ require "base64"
 #  ```
 #
 module PhcStringFormat
-
   module Formatter
-
-    #
     def self.format(id:, params: {}, salt: '', hash: '', hint: {salt: {encoding: 'base64'}})
       raise ArgumentError.new, 'id is required' if id.nil?
       raise ArgumentError.new, 'hash needs salt' if (salt.nil? || salt.empty?) && !(hash.nil? || hash.empty?)
 
       elements = [
         id,
-        (params.map {|e| e.join '='}.join(',') if params),
+        (params.map{|e| e.join '='}.join(',') if params),
         hint.dig(:salt, :encoding) == '7bit' ? salt : short_strict_encode64(salt),
         short_strict_encode64(hash)
       ]
-      "$#{elements.select {|e| !(e.nil? || e.empty?)} .join('$')}"
+      "$#{elements.select{|e| !(e.nil? || e.empty?)}.join('$')}"
     end
 
-    #
     def self.parse(string, hint: {salt: {encoding: 'base64'}})
       elements = string.split(/\$/, 5)
       elements.shift
@@ -34,11 +30,11 @@ module PhcStringFormat
       params = parse_parameter_string(elements.shift) if (elements.first || '').include?('=')
       salt = hint.dig(:salt, :encoding) == '7bit' ? elements.shift : short_strict_decode64(elements.shift)
       hash = short_strict_decode64(elements.shift)
-      {id: id, params: params, salt: salt, hash: hash }
+      {id: id, params: params, salt: salt, hash: hash}.select {|_,v| v}
     end
 
     def self.parse_parameter_string(string)
-      string.split(/,/).map {|e| e.split '='}.each_with_object({}) {|e, h| k, v = e; h[k.to_sym] = (/\A-?\d+(.\d+)?\Z/.match(v) ? v.to_i : v)}
+      string.split(/,/).map {|e| e.split '='}.each_with_object({}){|e, h| k, v = e; h[k] = (/\A-?\d+(.\d+)?\Z/.match(v) ? v.to_i : v)}
     end
 
     def self.short_strict_encode64(bin)
@@ -52,7 +48,5 @@ module PhcStringFormat
     end
 
     private_class_method :short_strict_encode64, :short_strict_decode64, :parse_parameter_string
-
   end
-
 end
