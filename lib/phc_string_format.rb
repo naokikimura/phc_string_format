39b7ba7d85a0d6802832bbc7e7f8f61992c21653
naokikimura <n.kimura.cap@gmail.com>
2018-07-29 00:23:06 +0900

    Refactor

--- lib/phc_string_format.rb
+++ lib/phc_string_format.rb
@@ -1,5 +1,5 @@
-require "phc_string_format/version"
-require "base64"
+require 'phc_string_format/version'
+require 'base64'
 
 #
 # PHC string format implemented by Ruby
@@ -8,64 +8,104 @@ require "base64"
 #  $<id>[$<param>=<value>(,<param>=<value>)*][$<salt>[$<hash>]]
 #  ```
 #
+# See:
+# - https://github.com/P-H-C/phc-string-format/blob/master/phc-sf-spec.md
+# - https://github.com/P-H-C/phc-string-format/pull/4
+#
 module PhcStringFormat
+  #
+  # Formatter for stringifying and parsing PHC-string-format.
+  #
   module Formatter
-    def self.format(id:, version: nil, params: {}, salt: '', hash: '', hint: {salt: {encoding: 'base64'}})
-      raise ArgumentError.new, 'id is required' if id.nil?
-      raise ArgumentError.new, 'hash needs salt' if (salt.nil? || salt.empty?) && !(hash.nil? || hash.empty?)
+    def self.format(**kwargs)
+      PhcString.create(kwargs).to_s
+    end
 
-      elements = [
+    def self.parse(string, hint: {}, pick: nil)
+      PhcString.parse(string, hint: hint).to_h(pick)
+    end
+  end
+
+  #
+  # Parser for parsing PHC-string-format.
+  #
+  class PhcString
+    @parse_version = ->(version) { @parse_parameters.call(version)['v'] }
+
+    @parse_parameters = lambda do |parameters|
+      mapper = ->(param) { param.split '=' }
+      reducer = lambda do |param, params|
+        name, value = param
+        params[name] = value =~ /\A-?\d+(.\d+)?\Z/ ? value.to_i : value
+      end
+      (parameters || '').split(/,/).map(&mapper).each_with_object({}, &reducer)
+    end
+
+    def self.parse(string, hint: {})
+      elements = (string || '').split(/\$/, 6)
+      elements.shift
+      id = elements.shift
+      version = @parse_version.call(elements.shift) if (elements.first || '').start_with?('v=')
+      params = @parse_parameters.call(elements.shift) if (elements.first || '').include?('=')
+      salt = elements.shift
+      hash = elements.shift
+      PhcString.new(id, version, params, salt, hash, hint)
+    end
+
+    def self.create(id:, version: nil, params: {}, salt: '', hash: '', hint: {})
+      PhcString.new(
         id,
         ("v=#{version}" if version),
-        (params.map{|e| e.join '='}.join(',') if params),
-        hint.dig(:salt, :encoding) == '7bit' ? salt : short_strict_encode64(salt),
-        short_strict_encode64(hash)
-      ]
-      "$#{elements.select{|e| !(e.nil? || e.empty?)}.join('$')}"
+        (params.map { |e| e.join '=' }.join(',') if params),
+        hint.dig(:salt, :encoding) == '7bit' ? salt : B64.encode(salt),
+        B64.encode(hash),
+        hint
+      )
     end
 
-    def self.parse(string, pick: nil ,hint: {salt: {encoding: 'base64'}})
-      pick = [:id, :version, :params, :salt, :hash] unless pick
-      elements = string.split(/\$/, 6)
-      elements.shift
-      id = shift_and_parse elements, pick.include?(:id), &-> (e) { e }
-      version = shift_and_parse(elements, pick.include?(:version), &@parse_version) if (elements.first || '').start_with?('v=')
-      params = shift_and_parse(elements, pick.include?(:params), &@parse_parameter_string) if (elements.first || '').include?('=')
-      salt = shift_and_parse elements, pick.include?(:salt), &-> (e) {
-        hint.dig(:salt, :encoding) == '7bit' ? e : short_strict_decode64(e)
-      }
-      hash = shift_and_parse elements, pick.include?(:hash), &-> (e) { short_strict_decode64(e) }
-      {id: id, version: version, params: params, salt: salt, hash: hash}.select {|_,v| v}
+    def initialize(id, version, params, salt, hash, hint)
+      raise ArgumentError.new, 'id is required' unless id
+      raise ArgumentError.new, 'hash needs salt' if (!salt || salt.empty?) && !(!hash || hash.empty?)
+
+      @id = id
+      @version = version
+      @params = params
+      @salt = salt
+      @hash = hash
+      @hint = hint
     end
 
-    def self.shift_and_parse(elements, picks, &proc)
-      element = elements.shift
-      proc.call(element) if picks
+    def to_s
+      '$' + [@id, @version, @params, @salt, @hash].reject { |e| e.nil? || e.empty? }.join('$')
     end
 
-    @parse_version = -> (string) {
-      @parse_parameter_string.call(string)['v']
-    }
+    def to_h(pick = nil)
+      pick ||= %i[id version params salt hash]
+      {
+        id: (@id if pick.include?(:id)),
+        version: (@version if pick.include?(:version)),
+        params: (@params if pick.include?(:params)),
+        salt: ((@hint.dig(:salt, :encoding) == '7bit' ? e : B64.decode(@salt)) if pick.include?(:salt)),
+        hash: (B64.decode(@hash) if pick.include?(:hash))
+      }.select { |_, v| v }
+    end
+  end
 
-    @parse_parameter_string = -> (string) {
-      (string || '').split(/,/)
-        .map { |param| param.split '=' }
-        .each_with_object({}) { |param, params|
-          name, value = param
-          params[name] = (/\A-?\d+(.\d+)?\Z/.match(value) ? value.to_i : value)
-        }
-    }
-
-    def self.short_strict_encode64(bin)
+  #
+  # Implementation of B64
+  #
+  # See:
+  # - https://github.com/P-H-C/phc-string-format/blob/master/phc-sf-spec.md#b64
+  #
+  module B64
+    def self.encode(bin)
       return nil unless bin
       Base64.strict_encode64(bin).delete('=')
     end
 
-    def self.short_strict_decode64(bin)
+    def self.decode(bin)
       return nil unless bin
       Base64.strict_decode64(bin + '=' * (-bin.size % 4))
     end
-
-    private_class_method :short_strict_encode64, :short_strict_decode64
   end
 end
